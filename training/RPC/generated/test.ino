d8d96c251d066699d4e7f703836f78243c4240d6
LFLCH <leofiloche.wk@gmail.com>
2023-12-11 12:54:04 +0100

    Adding some arduino generation

--- training/RPC/generated/test.ino
+++ training/RPC/generated/test.ino
@@ -1,8 +1,51 @@
-#include <PinChangeInt.h>     #include <PinChangeIntConfig.h>     #include <EEPROM.h>     #define _NAMIKI_MOTOR	 //for Namiki 22CL-103501PG80:1     #include <fuzzy_table.h>     #include <PID_Beta6.h>     #include <MotorWheel.h>     #include <Omni4WD.h>     
- irqISR(irq1, isr1);     MotorWheel wheel1(3, 2, 4, 5, &irq1);     
- irqISR(irq2, isr2);     MotorWheel wheel2(11, 12, 14, 15, &irq2);     
- irqISR(irq3, isr3);      
- MotorWheel wheel3(9, 8, 16, 17, &irq3);     
- irqISR(irq4, isr4);      MotorWheel wheel4(10, 7, 18, 19, &irq4);     
+#include <PinChangeInt.h>     
+#include <PinChangeIntConfig.h>     
+#include <EEPROM.h>     
+#define _NAMIKI_MOTOR	 //for Namiki 22CL-103501PG80:1     
+#include <fuzzy_table.h>     
+#include <PID_Beta6.h>     
+#include <MotorWheel.h>     
+#include <Omni4WD.h>     
+
+irqISR(irq1, isr1);     
+MotorWheel wheel1(3, 2, 4, 5, &irq1);     
+irqISR(irq2, isr2);     
+MotorWheel wheel2(11, 12, 14, 15, &irq2);     
+irqISR(irq3, isr3);      
+MotorWheel wheel3(9, 8, 16, 17, &irq3);     
+irqISR(irq4, isr4);     
+MotorWheel wheel4(10, 7, 18, 19, &irq4);     
+Omni4WD Omni(&wheel1, &wheel2, &wheel3, &wheel4);     
+
+void setup() {     
+  //TCCR0B=TCCR0B&0xf8|0x01;    // warning!! it will change millis()     
+  TCCR1B = TCCR1B & 0xf8 | 0x01; // Pin9,Pin10 PWM 31250Hz     
+  TCCR2B = TCCR2B & 0xf8 | 0x01; // Pin3,Pin11 PWM 31250Hz     
+   Omni.PIDEnable(0.31, 0.01, 0, 10);     
+}      
+    void moveInCircle(float radius, int speed)     
+{     
+float angularSpeed = speed / radius;     
+Omni.setVelocity(angularSpeed, 0, 0);     
+}     
+     
+void moveInSquare(float sideLength, int speed)     
+{     
+for (int i = 0; i < 4; ++i)     
+{     
+    Omni.setVelocity(0, speed, 0);     
+    delay(sideLength * 1000 / speed);     
+    Omni.setVelocity(0, 0, 0);     
+    delay(1000);     
+    Omni.setVelocity(0, 0, speed);     
+    delay(1000);     
+}     
+}     
+void loop() {     
+  Omni.demoActions(30,1500,500,false);     
+ moveInCircle(30, 1500);     
+ delay(500);     
  
- Omni4WD Omni(&wheel1, &wheel2, &wheel3, &wheel4);     void setup() {       //TCCR0B=TCCR0B&0xf8|0x01;    // warning!! it will change millis()       TCCR1B = TCCR1B & 0xf8 | 0x01; // Pin9,Pin10 PWM 31250Hz       TCCR2B = TCCR2B & 0xf8 | 0x01; // Pin3,Pin11 PWM 31250Hz        Omni.PIDEnable(0.31, 0.01, 0, 10);     }         void loop() {       Omni.demoActions(30,1500,500,false);     }
+ moveInSquare(500, 1500);     
+ delay(500);     
+}
--- training/RPC/src/generator/generator.ts
+++ training/RPC/src/generator/generator.ts
@@ -51,32 +51,53 @@ export function describeGame(game : Game): GameDescription {
 
 export function translateToArduino(game : Game) : string {
     let ino = "#include <PinChangeInt.h> \
-    #include <PinChangeIntConfig.h> \
-    #include <EEPROM.h> \
-    #define _NAMIKI_MOTOR	 //for Namiki 22CL-103501PG80:1 \
-    #include <fuzzy_table.h> \
-    #include <PID_Beta6.h> \
-    #include <MotorWheel.h> \
-    #include <Omni4WD.h> \
-    \n irqISR(irq1, isr1); \
-    MotorWheel wheel1(3, 2, 4, 5, &irq1); \
-    \n irqISR(irq2, isr2); \
-    MotorWheel wheel2(11, 12, 14, 15, &irq2); \
-    \n irqISR(irq3, isr3);  \
-    \n MotorWheel wheel3(9, 8, 16, 17, &irq3); \
-    \n irqISR(irq4, isr4); \
-    \ MotorWheel wheel4(10, 7, 18, 19, &irq4); \
-    \n \n Omni4WD Omni(&wheel1, &wheel2, &wheel3, &wheel4); \
-    void setup() { \
-      //TCCR0B=TCCR0B&0xf8|0x01;    // warning!! it will change millis() \
-      TCCR1B = TCCR1B & 0xf8 | 0x01; // Pin9,Pin10 PWM 31250Hz \
-      TCCR2B = TCCR2B & 0xf8 | 0x01; // Pin3,Pin11 PWM 31250Hz \
-       Omni.PIDEnable(0.31, 0.01, 0, 10); \
-    } \
-    \
-    void loop() { \
-      Omni.demoActions(30,1500,500,false); \
-    }";
+    \n#include <PinChangeIntConfig.h> \
+    \n#include <EEPROM.h> \
+    \n#define _NAMIKI_MOTOR	 //for Namiki 22CL-103501PG80:1 \
+    \n#include <fuzzy_table.h> \
+    \n#include <PID_Beta6.h> \
+    \n#include <MotorWheel.h> \
+    \n#include <Omni4WD.h> \
+    \n\nirqISR(irq1, isr1); \
+    \nMotorWheel wheel1(3, 2, 4, 5, &irq1); \
+    \nirqISR(irq2, isr2); \
+    \nMotorWheel wheel2(11, 12, 14, 15, &irq2); \
+    \nirqISR(irq3, isr3);  \
+    \nMotorWheel wheel3(9, 8, 16, 17, &irq3); \
+    \nirqISR(irq4, isr4); \
+    \nMotorWheel wheel4(10, 7, 18, 19, &irq4); \
+    \nOmni4WD Omni(&wheel1, &wheel2, &wheel3, &wheel4); \
+    \n\nvoid setup() { \
+    \n  //TCCR0B=TCCR0B&0xf8|0x01;    // warning!! it will change millis() \
+    \n  TCCR1B = TCCR1B & 0xf8 | 0x01; // Pin9,Pin10 PWM 31250Hz \
+    \n  TCCR2B = TCCR2B & 0xf8 | 0x01; // Pin3,Pin11 PWM 31250Hz \
+    \n   Omni.PIDEnable(0.31, 0.01, 0, 10); \
+    \n}  \
+    \n    void moveInCircle(float radius, int speed) \
+    \n{ \
+    \nfloat angularSpeed = speed / radius; \
+    \nOmni.setVelocity(angularSpeed, 0, 0); \
+    \n} \
+    \n \
+    \nvoid moveInSquare(float sideLength, int speed) \
+    \n{ \
+    \nfor (int i = 0; i < 4; ++i) \
+    \n{ \
+    \n    Omni.setVelocity(0, speed, 0); \
+    \n    delay(sideLength * 1000 / speed); \
+    \n    Omni.setVelocity(0, 0, 0); \
+    \n    delay(1000); \
+    \n    Omni.setVelocity(0, 0, speed); \
+    \n    delay(1000); \
+    \n} \
+    \n} \
+    \nvoid loop() { \
+    \n  Omni.demoActions(30,1500,500,false); \
+    \n moveInCircle(30, 1500); \
+    \n delay(500); \
+    \n \n moveInSquare(500, 1500); \
+    \n delay(500); \
+    \n}";
 
     return ino;
 }
